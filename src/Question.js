64cc38551a8c498be201edc5e41c47c7f1ea3dad
Murugesan <murugasiva3495@gmail.com>
2023-07-18 15:31:18 +0400

    Electrical Quiz app

--- src/Question.js
+++ src/Question.js
@@ -1,26 +1,171 @@
 export default [ {
-    questions :"hai",
-    option:['option 1 ','option 2','option 3','option 4'],
-    answer : "1"
+    questions :"1.Which of the following transformers is smallest ?",
+    option:['(a) 2 kVA 500 Hz ','(b) 2 kVA 400 Hz','(c) 2 kVA 200 Hz','(d) 2kVA 50 Hz'],
+    answer : "0"
 },
 {
-    questions :"hello ",
-    option:['otheropt 1 ','option 2','option 3','option 4'],
+    questions :"2.In a power or distribution transformer about 10 pm cent end turns are heavily insulated",
+    option:['(a) to withstand the high voltage drop due to line surge produced by the shunting capacitance of the end turns ',
+    '(b) to absorb the line surge voltage and save the winding of transformer from damage',
+    '(c) to reflect the line surge and save the winding of a transformer from damage',
+    '(d) none of the above'],
     answer : "0"
   
  
 },
 {
-    questions:"how are you",
-    option:['otheropt2 ','option 2','option 3','option 4'],
-    answer : "2"
+    questions:"3.Reduction in core losses and increase in permeability are obtained with transformer employing",
+    option:['(a)core built-up of laminations of cold rolled grain oriented steel ',
+    '(b) core built-up of laminations of hot rolled sheet',
+    '(c) either of the above',
+    '(d) none of the above'],
+    answer : "0"
 
 },
 
 { 
-    questions :"to all",
-    option:['otheropt3 ','option 2','option 3','option 4'],
+    questions :"4.The purpose of a breather in a transformer is to provide cooling to the winding",
+    option:['(a)take insulting oil from the conservator ',
+    '(b) provide insulation to the winding',
+    '(c) extract moisture from the air',
+    '(d) none of the above'],
+    answer : "2"
+},
+{ 
+    questions :"5.In core type transformers, the concentric windings are used with",
+    option:['(a)low voltage winding placed next to core ',
+    '(b) low voltage winding on the outer-side',
+    '(c) high voltage winding placed next to core',
+    '(d) high voltage winding on the outer-side'],
+    answer : "0"
+},
+{ 
+    questions :"6.The yoke sections of transformers using hot-rolled laminations is made 15 percentage greater than that of the core to",
+    option:['(a)to increase the size of the transformer',
+    '(b) to reduce the copper loss',
+    '(c) to reduce the iron loss in yoke and magnetizing current',
+    '(d) to provide better cooling'],
+    answer : "2"
+},
+{ 
+    questions :"7.When two single phase transformers are running in parallel and if the impedance triangles of the transformers are not identical in shape and size then",
+    option:['(a) power factor of one transformer and power factor of common load will be same.',
+    '(b) power factors at which the transformers operate will be different from one another and again these will be different from power factor of common load.',
+    '(c) power factors at which the transformers operate will be same but different from power factor of common load.',
+    '(d) power factors at which the transformers operate and power factor of common load all will be same.'],
+    answer : "1"
+},
+{ 
+    questions :"8.What is the typical use of an auto-transformer ?",
+    option:['(a) Control transformer',
+    '(b) Isolating transformer',
+    '(c) Variable transformer',
+    '(d) Toy transformer'],
+    answer : "2"
+},
+{ 
+    questions :"9.In a transformer the magnitude of mutual flux",
+    option:['(a) varies at low loads and constant at high loads',
+    '(b) is low at low loads and high at high loads',
+    '(c) is high at low loads and low at high loads',
+    '(d) same at all loads'],
+    answer : "4"
+},
+{ 
+    questions :"10.The principle of working of a transformer is",
+    option:['(a) mutual induction',
+    '(b) static induction',
+    '(c) self induction',
+    '(d) dynamic induction.'],
+    answer : "0"
+},
+{ 
+    questions :"11.In a transformer an insulating material may fail due to",
+    option:['(a) moisture',
+    '(b) dust',
+    '(c) voids in the winding',
+    '(d) any of the above'],
     answer : "3"
+},
+{ 
+    questions :"12.The reactance of a transformer depends on",
+    option:['(a) leakage flux ',
+    '(b) size of the core',
+    '(c) size of the tank',
+    '(d) all of the above.'],
+    answer : "0"
+},
+{ 
+    questions :"13.In a transformer on no load, the input voltage",
+    option:[
+    '(a) is always at 60 degrees to the magnetizing current',
+    '(b) is in phase with magnetizing current',
+    '(c) leads the magnetizing current by 90 degrees',
+    '(d) lags the magnetizing by 90 degrees.'],
+    answer : "2"
+},
+{ 
+    questions :"14.The value of useful flux least depends on",
+    option:[
+    '(a) load',
+    '(b) magneto-motive force',
+    '(c) voltage',
+    '(d) all of the above'],
+    answer : "0"
+},
+{ 
+    questions :"15.The type of load for which the voltage regulation of a transformer is negative is",
+    option:[
+    '(a) capacitive',
+    '(b) inductive',
+    '(c) resistive',
+    '(d) all of the above'],
+    answer : "0"
+},
+{ 
+    questions :"16.The magnetizing current for sinusoidal voltage applied, will be",
+    option:[
+    '(a) always sinusoidal',
+    '(b) always non-sinusoidal',
+    '(c) sinusoidal or non-sinusoidal depending upon the saturation point',
+    '(d) none of the above'],
+    answer : "2"
+},
+{ 
+    questions :"17.The transformation ratio of a transformer, for a given application",
+    option:[
+    '(a) depends on secondary load',
+    '(b) is constant but not fixed',
+    '(c) is fixed but not constant',
+    '(d) none of the above'],
+    answer : "1"
+},
+{ 
+    questions :"18.The output current corresponding to the maximum efficiency for a transformer having core loss of 200 W and equivalent resistance referred secondary of 0.5 ohms is",
+    option:[
+    '(a) 5 A',
+    '(b) 10 A',
+    '(c) 15A',
+    '(d) 20 A'],
+    answer : "3"
+},
+{ 
+    questions :"19. The saving in copper achieved by converting two winding transformer into an auto-transformer is determined by",
+    option:[
+    '(a) load on the secondary',
+    '(b) voltage transformation ratio',
+    '(c) size of the transformer core',
+    '(d) magnetic quality of core material'],
+    answer : "1"
+},
+{ 
+    questions :"20.While performing a short-circuit test on a transformer, usually low-voltage side is short-circuited because it has",
+    option:[
+    '(a) low insulation',
+    '(b) easy access',
+    '(c) lower terminal voltage and higher current rating',
+    '(d) more number of turns'],
+    answer : "2"
 }
 
 
